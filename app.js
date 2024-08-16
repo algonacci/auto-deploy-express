21010306abc4826480c14fa9fd6f1892df907bb1
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-08-16 21:06:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app = express();
 app.get("/", (req, res) => {
   res.status(200).json({
     status: {
-      message: "HELLO WORLD!",
+      message: "HELLO WORLD from eric, ini yang udah di-update!",
       code: 200,
     },
     data: null,
