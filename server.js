118658ee554be274e27eae7ee8d9a60a63d5dadc
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-08-16 18:18:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const app = require("./app");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 7002;
 
 const server = http.createServer(app);
 
